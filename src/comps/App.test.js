a86e309884e90259b04ab6ca39eba1f4b0157f83
NoraLou <nora.humpage@gmail.com>
2018-03-04 18:25:08 -0800

    change package.json to fix jest test runner issue

--- src/comps/App.test.js
+++ src/comps/App.test.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+console.log('shallow :', shallow)
 const app = shallow(<App />);
 
 it ('renders correctly', () => {
--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 import requestAnimationFrame from './tempPolyfills';
 
 import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapater-react-16';
+import Adapter from 'enzyme-adapter-react-16';
 
-configure({ adapter: new Adapter(), disableLifecycleMethods: true});
+
+configure({ adapter: new Adapter() });
