127bbc053027ec046e354ac1322660217b45e399
NoraLou <nora.humpage@gmail.com>
2018-03-04 20:58:40 -0800

    testing dom

--- src/comps/App.test.js
+++ src/comps/App.test.js
@@ -3,11 +3,30 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
-console.log('shallow :', shallow)
 const app = shallow(<App />);
 
 it ('renders correctly', () => {
   expect(app).toMatchSnapshot();
 });
+
+
+it ('initializes the state with an empty list of gifts', ()=> {
+  expect(app.state().gifts).toEqual([]);
+})
+
+
+it ('adds a new gift to the state when click the `add gift` button', ()=> {
+  app.find(`.btn-add`).simulate('click');
+  expect(app.state().gifts).toEqual([ {id:1} ]);
+})
+
+
+it('adds a new gift to the rendered list wehn clicking the `add gift button`', ()=> {
+  app.find('.btn-add').simulate('click')
+  expect(app.find('.gift-list').children().length).toEqual(2);
+})
+
+
+
 
 
